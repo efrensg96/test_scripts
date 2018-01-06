64e7acb894fb01f6faf2051144b3ff3d8a0b498e
efrensg96 <efren.sg96@gmail.com>
2018-01-05 18:27:14 -0600

    Update loop_time.py

--- loop_time.py
+++ loop_time.py
@@ -3,9 +3,15 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-t0 = time.time()
+arduinoPort = serial.Serial('COM1', 500000)
+dataSerial01 = []
 flag = False
+time.sleep(3)
+print "start time"
+t0 = time.time()
 while not flag:
     flag = True
+    dataSerial01.append(arduinoPort.readline().strip())
     t1 = time.time()
 print "time per loop", np.float(t1 - t0)
+print "dataSerial01: ", dataSerial01
