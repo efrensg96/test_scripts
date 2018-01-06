7eedf73e990c913d4c7a9ae349f4a29708df6c7a
efrensg96 <efren.sg96@gmail.com>
2018-01-05 19:07:52 -0600

    Update loop_time.py

--- loop_time.py
+++ loop_time.py
@@ -1,9 +1,3 @@
-import serial
-import time
-import numpy as np
-import matplotlib.pyplot as plt
-
-arduinoPort = serial.Serial('/dev/ttyUSB0', 500000) # to read
 dataSerial01 = []
 flag = False
 time.sleep(3)
@@ -11,10 +5,10 @@ counter = 0
 print "start time"
 t0 = time.time()
 while True:
-    if counter >= 44100:
+    if counter >= 8000:
         break
-    # dataSerial01.append(arduinoPort.readline().strip())
+    dataSerial01.append(arduinoPort.readline().strip())
     counter += 1
 print "time per loop", np.float(time.time() - t0)
 print "counter: ", counter
-#print "dataSerial01: ", dataSerial01
+print "dataSerial01 lenght: ", len(dataSerial01)
