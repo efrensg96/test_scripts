cc1b7a94b0eb64cd1c79c845116cf9aa83514c03
efrensg96 <efren.sg96@gmail.com>
2018-02-05 14:13:18 +0100

    Update I2S_example_01.py

--- I2S_example_01.py
+++ I2S_example_01.py
@@ -11,16 +11,15 @@ import wave
 def main():
 
     arduinoPort = serial.Serial('/dev/ttyACM0', 1000000, timeout=3)
-    dataSerial_01 = []    
+    dataSerial_01 = []
     CHUNK = 96000 #2 channels, 16khz of fs, time to be recorded (2*16000*time) = chunk size
-    N = 2**16
 
     t0 = time.time()
     print "Start time"
 
     while True:
         t1 = time.time()
-        if (t1 - t0) >= 1.0:
+        if (t1 - t0) >= 5.0:
             break
         dataSerial_01.append(struct.unpack("i", arduinoPort.read(4)))
 
@@ -29,7 +28,6 @@ def main():
     print "dataSerial_01: ", dataSerial_01[0:20]
 
     data01 = np.int32(dataSerial_01)
-    data01 = np.int16(np.round(data01/N))
 
     print "data01 shape: ", data01.shape
 
@@ -49,8 +47,7 @@ def main():
     WAVE_OUTPUT_FILENAME = "I2S_example_01.wav"
     CHANNELS = 2
     RATE = 16000
-    FORMAT = pyaudio.paInt16
-#     FORMAT = pyaudio.paInt32
+    FORMAT = pyaudio.paInt32
 
     p = pyaudio.PyAudio()
 
